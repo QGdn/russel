4344b9f109bef9efad50f67fc58a6ba9fc450b4b
QGdn <quentin.gaudin78@gmail.com>
2025-02-21 08:53:06 +0100

    authenticate

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const service = require('../services/users');
+
+const private = require('../middlewares/private');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:id', private.checkJWT, service.getById);
+router.put('/add', service.add);
+router.patch('/:id', private.checkJWT, service.update);
+router.delete('/:id', private.checkJWT, service.delete);
+router.post('/authenticate', service.authenticate);
 
 module.exports = router;
